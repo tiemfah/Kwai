66ac8809d8128fc2163a2a6f76eba26143ab88e2
tiemfah <sivanat.sub@gmail.com>
2019-03-25 22:36:31 +0700

    cleaning1

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import arcade
 from models import *
- 
+
 GRID = 32
+
 SCREEN_WIDTH = GRID * 9
 SCREEN_HEIGHT = GRID * 18
 VIEWPORT_MARGIN = GRID * 8
@@ -10,13 +11,13 @@ VIEWPORT_MARGIN = GRID * 8
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
         self.model = kwargs.pop('model', None)
- 
+
         super().__init__(*args, **kwargs)
- 
+
     def sync_with_model(self):
         if self.model:
             self.set_position(self.model.x, self.model.y)
- 
+
     def draw(self):
         self.sync_with_model()
         super().draw()
@@ -32,62 +33,61 @@ class LevelGenerator():
         self.dirt_sprite = arcade.Sprite('resources/images/dirt.png')
         self.stone_sprite = arcade.Sprite('resources/images/stone.png')
 
-    def get_sprite_pos(self, r, c):
+    def get_sprite_position(self, r, c):
         x = (c * GRID + (GRID // 2))
-        y = SCREEN_HEIGHT- ((r-1) * GRID + (GRID + (GRID // 2)))
-        return x,y
-    
+        y = SCREEN_HEIGHT - ((r - 1) * GRID + (GRID + (GRID // 2)))
+        return x, y
+
     def draw_sprite(self, sprite, r, c):
-        x, y = self.get_sprite_pos(r, c)
+        x, y = self.get_sprite_position(r, c)
         sprite.set_position(x, y)
         sprite.draw()
-    
+
     def draw(self):
         for r in range(self.height):
             for c in range(self.width):
-                iden =  self.level.what_is_at(r, c)
-                if iden == "sky":
+                identity = self.level.what_is_at(r, c)
+                if identity == "sky":
                     self.draw_sprite(self.sky_sprite, r, c)
-                elif iden == "dirt":
+                elif identity == "dirt":
                     self.draw_sprite(self.dirt_sprite, r, c)
-                elif iden == "stone":
+                elif identity == "stone":
                     self.draw_sprite(self.stone_sprite, r, c)
-    
+
     def update(self, Levelmap):
         self.level = Levelmap
         self.width = self.level.width
         self.height = self.level.height
- 
+
 
 class GameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
- 
+
         arcade.set_background_color(arcade.color.WHITE)
-        
+
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.player = ModelSprite('resources/images/mc.png',
-                                         model=self.world.player)
+                                  model=self.world.player)
 
         self.map = LevelGenerator(self.world.level)
         self.view_bottom = 0
-    
+
     def change_view(self):
         changed = True
         top_bndry = self.view_bottom + SCREEN_HEIGHT - VIEWPORT_MARGIN
-        if self.world.player.y+16 > top_bndry:
-            self.view_bottom += self.world.player.y+16 - top_bndry
+        if self.world.player.y + 16 > top_bndry:
+            self.view_bottom += self.world.player.y + 16 - top_bndry
             changed = True
 
         # Scroll down
         bottom_bndry = self.view_bottom + VIEWPORT_MARGIN
-        if self.world.player.y-16 < bottom_bndry:
-            self.view_bottom -= bottom_bndry - self.world.player.y-16
+        if self.world.player.y - 16 < bottom_bndry:
+            self.view_bottom -= bottom_bndry - self.world.player.y - 16
             changed = True
 
         if changed:
-            arcade.set_viewport(0,
-                                SCREEN_WIDTH,
+            arcade.set_viewport(0, SCREEN_WIDTH,
                                 self.view_bottom,
                                 SCREEN_HEIGHT + self.view_bottom)
 
@@ -95,21 +95,20 @@ class GameWindow(arcade.Window):
         self.world.update(delta)
         self.change_view()
         self.map = LevelGenerator(self.world.level)
-        
- 
+
     def on_draw(self):
         arcade.start_render()
-        
+
         self.map.draw()
         self.player.draw()
 
         # show score
         output = f"Score: {self.world.player.score}"
-        arcade.draw_text(output, SCREEN_WIDTH//2, SCREEN_HEIGHT//2, arcade.color.RED, 14)
-    
+        arcade.draw_text(output, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, arcade.color.RED, 14)
+
     def on_key_press(self, key, modifiers):
         self.world.on_key_press(key, modifiers)
-    
+
     def on_key_release(self, key, modifiers):
         self.world.on_key_release()
 
@@ -118,6 +117,7 @@ def main():
     window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.set_window(window)
     arcade.run()
- 
+
+
 if __name__ == '__main__':
     main()
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-import arcade
-import random
-import math
+import arcade.key
+from random import choice
+from math import ceil, floor
 
 GRID = 32
 
@@ -9,17 +9,17 @@ DIR_UP = 1
 DIR_RIGHT = 2
 DIR_DOWN = 3
 DIR_LEFT = 4
- 
-DIR_OFFSETS = { DIR_STILL: (0,0),
-                DIR_UP: (0,-1),
-                DIR_RIGHT: (1,0),
-                DIR_DOWN: (0,1),
-                DIR_LEFT: (-1,0) }
 
-KEY_MAP = { arcade.key.W: DIR_UP,
-            arcade.key.S: DIR_DOWN,
-            arcade.key.A: DIR_LEFT,
-            arcade.key.D: DIR_RIGHT, }
+DIR_OFFSETS = {DIR_STILL: (0, 0),
+               DIR_UP: (0, -1),
+               DIR_RIGHT: (1, 0),
+               DIR_DOWN: (0, 1),
+               DIR_LEFT: (-1, 0)}
+
+KEY_MAP = {arcade.key.W: DIR_UP,
+           arcade.key.S: DIR_DOWN,
+           arcade.key.A: DIR_LEFT,
+           arcade.key.D: DIR_RIGHT, }
 
 
 class Player:
@@ -29,42 +29,37 @@ class Player:
     can fall 
     """
     MOVE_WAIT = 0.1
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
         self.wait_time = 0
         self.direction = DIR_STILL
-        self.next_direction = DIR_STILL  # check off here
+        self.next_direction = DIR_STILL
         self.GRID = GRID
         self.starting_point = y
         self.score = 0
-        
-    
+
     def move(self, direction):
         self.x += GRID * DIR_OFFSETS[direction][0]
         self.y -= GRID * DIR_OFFSETS[direction][1]
- 
+
     def update(self):
         if self.next_direction != DIR_STILL:
             if self.check_moveable(self.next_direction):
                 self.direction = self.next_direction
                 self.next_direction = DIR_STILL
                 self.move(self.direction)
-        self.score = (self.starting_point - self.y)//32
-    
+        self.score = (self.starting_point - self.y) // 32
+
     def get_row(self):
-        # print(18 - math.ceil(self.y/self.GRID))
-        return  18 - math.ceil(self.y/self.GRID)
- 
+        return 18 - ceil(self.y / self.GRID)
+
     def get_col(self):
-        # print(math.floor(self.x /self.GRID))
-        return math.floor(self.x /self.GRID)
-    
-    def check_moveable(self, next_direction):  # check off here
-        """
-        HERERHERHERHER
-        """
+        return floor(self.x / self.GRID)
+
+    def check_moveable(self, next_direction):
         # print('current', self.get_row(), self.get_col())
         new_r = self.get_row() + DIR_OFFSETS[self.next_direction][1]
         new_c = self.get_col() + DIR_OFFSETS[self.next_direction][0]
@@ -78,6 +73,7 @@ class Dirt:
     can be destroy by player
     can block player
     """
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
@@ -90,6 +86,7 @@ class Nondirt:
     can block player
     can have many "skin"
     """
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
@@ -108,10 +105,11 @@ class Level:
                    '$#DDD..#$',
                    '$#.DDDD#$',
                    '$#DD.DD#$',
-                   '$#D.DDD#$',]
+                   '$#D.DDD#$', ]
     """
     make platfrom
     """
+
     def __init__(self, worldx):
         self.world = worldx
         self.map = self.choose_map() + \
@@ -124,20 +122,19 @@ class Level:
                    self.choose_map() + \
                    self.choose_map()
 
-                    
         self.height = len(self.map)
         self.width = len(self.map[0])
         self.previous_score = self.world.player.score
-    
+
     def choose_map(self):
         """
         pick random map that gives 2 block
         """
         if self.world.player.score < Level.LEV_1_CAP:
-            return [Level.level_1_map[0] , random.choice(Level.level_1_map)]
+            return [Level.level_1_map[0], choice(Level.level_1_map)]
         else:
-            return [Level.level_2_map[0] , random.choice(Level.level_2_map)]
-    
+            return [Level.level_2_map[0], choice(Level.level_2_map)]
+
     def what_is_at(self, r, c):
         """
         name implies
@@ -152,34 +149,30 @@ class Level:
             return "stone"
         elif self.map[r][c] == ".":
             return "air"
-        
-    
+
     def update(self):
         """
         see if map need new "block"
-        TODO check player score and pop beginning map and apppend new map at end
-
-        THIS SHOULD WORK HAVEN'T TEST THIS
         """
         if self.previous_score + 1 == self.world.player.score:
             self.map += self.choose_map()
             self.previous_score = self.world.player.score
-        
- 
+
+
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.player = Player(self, GRID//2+ 3*GRID, GRID*17+GRID//2)
+        self.player = Player(self, GRID // 2 + 3 * GRID, GRID * 17 + GRID // 2)
         self.level = Level(self)
 
     def update(self, delta):
         self.player.update()
         self.level.update()
-    
+
     def on_key_press(self, key, key_modifiers):
         if key in KEY_MAP:
             self.player.next_direction = KEY_MAP[key]
-    
+
     def on_key_release(self):
         self.player.direction = DIR_STILL
